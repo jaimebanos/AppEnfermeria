35af218c1feb5d5e8c706961e79ca665d9d1abf7
jaimebanos <jaimebanossoriano@gmail.com>
2022-10-29 15:16:50 +0200

    Auth-inc
    Preparar el auth para su futuro uso, falta probar su funcionamiento

--- php/auth_inc.php
+++ php/auth_inc.php
@@ -5,6 +5,11 @@
     if(!$token == null){
         //TODO hacer referencia a una función de Usuarios que comprueba si ese token que recibe existe, o se ha caducado
         // si no existe, se devuelve un echo con succes false y se le dirige al login
+        if(!Usuarios::comprobar_token($token)){
+
+            echo json_encode(array("data"=>"","msg"=>"","success"=>false));
+        }
     }else{
-        //TODO si está en nulo, se hará un echo con succes false y se le dirige al login
-    }
+        //Si token es null, te devuelve al login
+        echo json_encode(array("data"=>"","msg"=>"","success"=>false));
+}
