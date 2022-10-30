b4081eb2ccf93c30db4a8cb539e469f38b413fe1
KwAhmama <kawtar.ahmama@gmail.com>
2022-10-30 11:40:41 +0100

    Cambios sintaxis

--- php/Usuarios.php
+++ php/Usuarios.php
@@ -1,5 +1,6 @@
 <?php
 include "..\\Conexion\\ConexionSingle.php";
+
 class Usuarios
 {
     public $nombre;
@@ -14,8 +15,8 @@ class Usuarios
 
     public function __construct($dni, $contrasenya)
     {
-            $this->dni = $dni;
-            $this->contrasenya = $contrasenya;
+        $this->dni = $dni;
+        $this->contrasenya = $contrasenya;
     }
 
     /**
@@ -24,31 +25,32 @@ class Usuarios
      * @return mixed|void
      * @throws Exception
      */
-    public function login(){
+    public function login()
+    {
 
         #CONEXION SINGLE
         $conexion = ConexionSingle::getInstancia();
-        $token=null;
+        $token = null;
         #BUSCA EN LA BASE DE DATOS EL DNI INTRUCIDO Y COMPRUEBA SI LAS PASSWORD COINCIDE
         try {
             $sql = "select * from usuario where dni = '$this->dni'";
             $sth = $conexion->prepare($sql);
             $sth->execute();
             $registro = $sth->fetch(PDO::FETCH_ASSOC);
-            if($registro != false){
-                if($registro['contrasenya']== sha1($this->contrasenya)) {
+            if ($registro != false) {
+                if ($registro['contrasenya'] == sha1($this->contrasenya)) {
                     $existe = self::token_exist($this->dni);
-                    if(!$existe){
-                        $token = $this->crear_token($this->dni,$this->contrasenya);
-                        $datos_devolver = array('token'=>$token);
+                    if (!$existe) {
+                        $token = $this->crear_token($this->dni, $this->contrasenya);
+                        $datos_devolver = array('token' => $token);
                         return $datos_devolver;
-                    }else{
+                    } else {
                         return $existe;
                     }
                 }
             }
-        }catch (Exception $e){
-            Throw($e);
+        } catch (Exception $e) {
+            throw($e);
         }
 
 
@@ -60,13 +62,14 @@ class Usuarios
      * @param $contrasenya
      * @return void
      */
-    private function crear_token($dni,$contrasenya){
+    private function crear_token($dni, $contrasenya)
+    {
 
         //CREAER EL TOKEN CON DNI Y PASSWORD DEL USUARIO
         $options = [
             'cost' => 11
         ];
-        $token = password_hash($dni.$contrasenya,PASSWORD_BCRYPT,$options);
+        $token = password_hash($dni . $contrasenya, PASSWORD_BCRYPT, $options);
 
 
         $conexion = ConexionSingle::getInstancia();
@@ -74,17 +77,18 @@ class Usuarios
             $sql = "UPDATE usuario set token = '$token' where dni = '$dni'";
             $sth = $conexion->prepare($sql);
             $sth->execute();
-        }catch (Exception $e){
-            Throw $e;
+        } catch (Exception $e) {
+            throw $e;
         }
 
         return $token;
     }
 
     /**
-    * Te devuelve el dni y el nombre del token pasado por parametro, si no tienes token, se te devolverá al login..
-    */
-    public static function comprobar_token($var_token){
+     * Te devuelve el dni y el nombre del token pasado por parametro, si no tienes token, se te devolverá al login..
+     */
+    public static function comprobar_token($var_token)
+    {
         $conexion = ConexionSingle::getInstancia();
         $token = $var_token;
         try {
@@ -92,64 +96,65 @@ class Usuarios
             $sth = $conexion->prepare($sql);
             $sth->execute();
             $campo_token = $sth->fetch(PDO::FETCH_ASSOC);
-            if($campo_token != false){
-                if ($token == $campo_token['token']){
+            if ($campo_token != false) {
+                if ($token == $campo_token['token']) {
                     return true;
                 }
             }
-        }catch (Exception $e){
-            Throw $e;
+        } catch (Exception $e) {
+            throw $e;
         }
 
     }
 
-    public static function token_exist($dni){
-     $conexion = ConexionSingle::getInstancia();
-      try {
-         $sql = "select token from usuario where dni = '$dni'";
-         $sth = $conexion->prepare($sql);
-         $sth->execute();
-         $token = $sth->fetch(PDO::FETCH_ASSOC); 
-         if($token['token'] == ""){
-             return false;
-         }else{
-             return $token;    
-         }
-     }catch (Exception $e){
-          Throw $e;
-     }
-        
-   }
+    public static function token_exist($dni)
+    {
+        $conexion = ConexionSingle::getInstancia();
+        try {
+            $sql = "select token from usuario where dni = '$dni'";
+            $sth = $conexion->prepare($sql);
+            $sth->execute();
+            $token = $sth->fetch(PDO::FETCH_ASSOC);
+            if ($token['token'] == "") {
+                return false;
+            } else {
+                return $token;
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+    }
 
 
-    public static function mostrarInfo($token){
+    public static function mostrarInfo($token)
+    {
         $pdo = ConexionSingle::getInstancia();
         try {
-            $sql = "SELECT a.*, FLOOR(DATEDIFF(NOW(),Fecha_nacimiento)/365) AS edad from alumno a, usuario u where u.token ='$token' and dni = id_usuario";
+            $sql = "SELECT a.*, FLOOR(DATEDIFF(NOW(),Fecha_nacimiento)/365) AS edad from admin a, usuario u where u.token ='$token' and dni = id_usuario";
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
             $data = $stmt->fetchAll();
 
-            /**
-             *    if (empty($data)) {
-            $sql = "SELECT * FROM admin a, usuario u where a.id_usario = u.dni and u.token ='$token'   ";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
             if (empty($data)) {
-            $sql = "SELECT * FROM profesor a, usuario u where a.id_usario = u.dni and u.token ='$token'   ";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
+                $sql = "SELECT a.*, FLOOR(DATEDIFF(NOW(),Fecha_nacimiento)/365) AS edad from profesor a, usuario u where u.token ='$token' and dni = id_usuario";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $data = $stmt->fetchAll();
+                if (empty($data)) {
+                    $sql = "SELECT a.*, FLOOR(DATEDIFF(NOW(),Fecha_nacimiento)/365) AS edad from alumno a, usuario u where u.token ='$token' and dni = id_usuario";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute();
+                    $data = $stmt->fetchAll();
 
+                }
             }
-            }
-             */
+
 
             return $data;
 
-        }catch (Exception $e){
-            Throw $e;
+        } catch (Exception $e) {
+            throw $e;
         }
     }
 
