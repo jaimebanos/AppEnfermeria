2f6d20ecc624bfc57b0ccf0dca4572f213255f4e
KwAhmama <kawtar.ahmama@gmail.com>
2022-11-02 16:22:18 +0100

    Corrección de enlaces

--- js/ajax_list_pacientes_edit.js
+++ js/ajax_list_pacientes_edit.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
-    $(document).on('click', '.eliminar_paciente', function(){
+    $(document).on('click', '.editar_paciente', function(){
         let id =$(this).parent().parent().children().first().text();
 
-        $(document).on('click', '.eliminar_paciente_confirm', function(){
 
-            var request = $.ajax({
-                url: '../php/wb_Cpacientes.php',
-                method: "POST",
-                data: {
-                    "accion": "editar_paciente",
-                    'token': sessionStorage.getItem('token'),
-                    'id_editar': id,
-                },
-                dataType: "json"
-            });
+        console.log(id);
+        var request = $.ajax({
+            url: '../php/wb_Cpacientes.php',
+            method: "POST",
+            data: {
+                "accion": "editar_paciente",
+                'token': sessionStorage.getItem('token'),
+                'id_editar': id,
+            },
+            dataType: "json"
+        });
 
-            request.done(function (response) {
-            });
+        request.done(function (response) {
+        });
 
-            request.fail(function (jqXHR, textStatus) {
-                console.log("Error alumnos");
-                alert("Request failed: " + textStatus);
-            });
+        request.done(function (response) {
+            /**
+             * Ir a editar perfil rellenando los campos
+             */
+        });
 
-        })
-    });
+        request.fail(function (jqXHR, textStatus) {
+            console.log("Error alumnos");
+            alert("Request failed: " + textStatus);
+        });
 
+    })
 });
+
+
+
