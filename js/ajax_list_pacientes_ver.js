16d25b2ec4c234b2999023516b00ee8be0440bd0
jaime <jaimebanos@gmail.com>
2022-11-16 18:18:49 +0100

    Error ruta Absoluta
    Error de ruta absouta al ver perfil de un paciente

--- js/ajax_list_pacientes_ver.js
+++ js/ajax_list_pacientes_ver.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
         let telefono =$(this).parent().parent().children().first().text();
 
         sessionStorage.setItem("telefono",telefono);
-        window.location.href="path/localhost/AppEnfermeria/html/login.html";
+        window.location.href="/AppEnfermeria/html/perfil_pacientes.html";
     });
 
     $(document).on('click', '.ver_paciente_index', function(){
         let telefono =$(this).parent().parent().children().first().text();
 
         sessionStorage.setItem("telefono",telefono);
-        window.location.href="/AppEnfermeria/html/login.html";
+        window.location.href="/AppEnfermeria/html/perfil_pacientes.html";
     });
 });
 
